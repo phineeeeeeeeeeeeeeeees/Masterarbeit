ccd7b42fb1c38db4ddfee81427d6aeb7f5cfd8f4
phineeeeeeeeeeeeeeeees_MacBook <b04801015@g.ntu.edu.tw>
2021-08-17 11:26:49 +0200

    20210817 Daily LGB grid search

--- 2_scripts/daily_lgb_gridsearch.R
+++ 2_scripts/daily_lgb_gridsearch.R
@@ -77,71 +77,60 @@ if(SAT_product == "OMI"){
 # =====================================
 response_full <- data_daily %>% 
   select(NO2) %>% 
-  unlist() %>% unname()
+  as.matrix()
 
 # =====================================
 # predictor
 # screening of important predictor variables
 # =====================================
-# set.seed(123)
-# lgb_screen <- lightgbm(
-#   data = data_daily %>% 
-#     select(-all_of(columns_nonpredictor)) %>% 
-#     # random-value variables
-#     mutate(R1 = runif(n()) , 
-#            R2 = runif(n()) ,
-#            R3 = runif(n()) ) %>% 
-#     # matrix
-#     as.matrix() %>% 
-#     # lgb.Dataset object
-#     lgb.Dataset(data = . , label = response_full) , 
-#   params = list(
-#     learning_rate = 0.05 , 
-#     bagging_fraction = 0.8 , 
-#     max_depth = 8
-#   ) ,
-#   objective = "regression" ,
-#   boosting_type = "gbdt" , 
-#   nrounds = 100
-#   )
-# 
-# # importance
-# lgb_screen_importance <- lgb_screen %>% 
-#   lgb.importance(model = .) %>% 
-#   as_tibble()
-# # visualization
-# lgb_screen_importance %>% 
-#   mutate(class = ifelse(str_detect(Feature , "R[123]") , "Random" , "Predictor variables")) %>% 
-#   # reorder for visualization
-#   mutate(Feature = factor(Feature , levels = Feature[order(Gain)])) %>% 
-#   # visualization
-#   ggplot(aes(x = Feature , y = Gain , fill = class)) +
-#   geom_bar(stat = "identity") +
-#   coord_flip() +
-#   scale_fill_lancet() +
-#   labs(x = "Variables" , y = "Variable importance (Gain)" , fill = "" , 
-#        title = "Screening of relevant predictor variables" , 
-#        subtitle = sprintf("The variable importance of LightGBM (%s)" , SAT_product)) +
-#   theme_bw() +
-#   theme(axis.text.y = element_text(size = 4) , legend.position = "bottom")
-# 
-# # exclude not-important variables 
-# included_var <- xgb_screen_importance %>% 
-#   filter(Gain > xgb_screen_importance %>% 
-#            filter(str_detect(Feature , "R[123]")) %>% 
-#            # the max importance of the random-value variables 
-#            summarize(Gain = max(Gain)) %>% 
-#            unlist) %>% 
-#   select(Feature) %>% 
-#   unlist %>% unname()
+set.seed(123)
+lgb_screen <- lightgbm(
+  data = data_daily %>%
+    select(-all_of(columns_nonpredictor)) %>%
+    # random-value variables
+    mutate(R1 = runif(n()) ,
+           R2 = runif(n()) ,
+           R3 = runif(n()) ) %>%
+    # matrix
+    as.matrix() %>%
+    # lgb.Dataset object
+    lgb.Dataset(data = . , label = response_full) ,
+  # default
+  params = list(learning_rate = 0.1 , num_leaves = 31) ,
+  objective = "regression" ,
+  boosting_type = "gbdt" ,
+  nrounds = 500
+  )
 
-# =====================================
-# predictor variables (as a matrix)
-# =====================================
-predictor_full <- data_daily %>% 
-  select(-all_of(columns_nonpredictor)) %>% 
-  #select(all_of(included_var)) %>% 
-  as.matrix()
+# importance
+lgb_screen_importance <- lgb_screen %>%
+  lgb.importance(model = .) %>%
+  as_tibble()
+# visualization
+lgb_screen_importance %>%
+  mutate(class = ifelse(str_detect(Feature , "R[123]") , "Random" , "Predictor variables")) %>%
+  # reorder for visualization
+  mutate(Feature = factor(Feature , levels = Feature[order(Gain)])) %>%
+  # visualization
+  ggplot(aes(x = Feature , y = Gain , fill = class)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  scale_fill_lancet() +
+  labs(x = "Variables" , y = "Variable importance (Gain)" , fill = "" ,
+       title = "Screening of relevant predictor variables" ,
+       subtitle = sprintf("The variable importance of LightGBM (%s)" , SAT_product)) +
+  theme_bw() +
+  theme(axis.text.y = element_text(size = 4) , legend.position = "bottom")
+
+# exclude not-important variables
+included_var <- lgb_screen_importance %>%
+  filter(Gain > lgb_screen_importance %>%
+           filter(str_detect(Feature , "R[123]")) %>%
+           # the max importance of the random-value variables
+           summarize(Gain = max(Gain)) %>%
+           unlist) %>%
+  select(Feature) %>%
+  unlist %>% unname()
 
 # //////////////////////////////////////////////////////////////////////////
 # grid search for best model hyperparameters
@@ -166,12 +155,14 @@ CV_folds <- data_daily %>%
 # create hyperparameter grid
 hyper_grid <- expand.grid(
   stringsAsFactors = FALSE , 
+  # variable selection
+  selection = c(TRUE , FALSE) , 
   # learning rate
   learning_rate = c(.01, .05, .1, .3),
   # tree depth
   max_depth = c(1, 3, 5, 8, 10),
   # number of leaves
-  num_leaves = c(5,25,50,100,200),
+  num_leaves = c(25,50,100,200),
   # bagging fraction
   bagging_fraction = c(0.5,0.7,0.85,1) ,
   # feature fraction
@@ -181,9 +172,9 @@ hyper_grid <- expand.grid(
   feature_fraction = c(0.5,0.75,1) ,
   # booster
   boosting_type = c("gbdt" , "dart") ,
-  optimal_trees = NA,               # a place to dump results
-  min_RMSE = NA,                    # a place to dump results
-  CV_R2 = NA                        # a place to dump results
+  # a place to dump results
+  optimal_trees = NA,               
+  min_RMSE = NA
 )
 
 # =====================================
@@ -192,44 +183,41 @@ hyper_grid <- expand.grid(
 pb <- txtProgressBar(min = 1 , max = nrow(hyper_grid) , style = 3 )
 for(i in 1:nrow(hyper_grid)) {
   # create parameter list
-  hyper_i <- list(
-    learning_rate = hyper_grid$learning_rate[i] , 
-    max_depth = hyper_grid$max_depth[i] , 
-    num_leaves = hyper_grid$num_leaves[i] , 
-    bagging_fraction = hyper_grid$bagging_fraction[i] , 
-    feature_fraction = hyper_grid$bagging_fraction[i] , 
-    boosting_type = hyper_grid$boosting[i]
-  )
+  hyper_i <- hyper_grid %>% 
+    dplyr::slice(i) %>% 
+    select(-optimal_trees , -min_RMSE) %>% 
+    as.list()
+  # variable selection
+  if(hyper_i$selection){
+    predictor_full <- data_daily %>% 
+      select(all_of(included_var)) %>% 
+      as.matrix()
+  }else{
+    predictor_full <- data_daily %>% 
+      select(-all_of(columns_nonpredictor)) %>% 
+      as.matrix()
+  }
   # reproducibility
   set.seed(123)
+  # cross validation
   lgb_grid <- lgb.cv(
     params = hyper_i , 
-    data = data_daily %>%
-      select(-all_of(columns_nonpredictor)) %>%
-      # matrix
-      as.matrix() %>%
-      # lgb.Dataset object
-      lgb.Dataset(data = . , label = response_full) ,
-    # label = response_full ,
+    data = predictor_full , 
+    label = response_full ,
     nrounds = 1000 , 
     objective = "regression" , 
     eval = "rmse" ,
     task = "prediction" ,
     folds = CV_folds , 
     showsd = FALSE , 
-    verbose = 1 , # silent
-    early_stopping_rounds = 10 # stop if no improvement for 10 consecutive trees
+    verbose = -1 , # silent
+    early_stopping_rounds = 50 # stop if no improvement for 10 consecutive trees
   )
-  lgb_grid$best_iter
-  lgb_grid$record_evals$valid$rmse$eval %>% unlist %>% which.min()
-  lgb_grid$record_evals$valid$rmse$eval %>% unlist %>% plot()
   # evaluation (add to hyper_grid data.frame)
   # the number of iterations with the lowest CV-RMSE
-  hyper_grid$optimal_trees[i] <- which.min(xgb_grid$evaluation_log$test_rmse_mean)
+  hyper_grid$optimal_trees[i] <- lgb_grid$record_evals$valid$rmse$eval %>% unlist %>% which.min()
   # CV-RMSE
-  hyper_grid$min_RMSE[i] <- min(xgb_grid$evaluation_log$test_rmse_mean)
-  # CV-R2
-  hyper_grid$CV_R2[i] <- cor(response_full , xgb_grid$pred)^2
+  hyper_grid$min_RMSE[i] <- lgb_grid$record_evals$valid$rmse$eval %>% unlist %>% min()
   # progress bar
   setTxtProgressBar(pb,i)
   # clean environment
@@ -237,29 +225,11 @@ for(i in 1:nrow(hyper_grid)) {
 }
 rm(pb,i)
 
-lgb_test <- lightgbm(
-  params = hyper_i , 
-  data = data_daily %>%
-    select(-all_of(columns_nonpredictor)) %>%
-    # matrix
-    as.matrix() %>%
-    # lgb.Dataset object
-    lgb.Dataset(data = . , label = response_full) ,
-  # label = response_full ,
-  nrounds = 1000 , 
-  objective = "regression" , 
-  task = "train" , 
-  #metric = "rmse" , 
-  verbose = 1 , # silent
-  early_stopping_rounds = 10 # stop if no improvement for 10 consecutive trees
-)
 
-pred_test <- predict(lgb_test , predictor_full)
-plot(pred_test , response_full)
 # =====================================
 # export grid search results
 # =====================================
-out_dirpath_hypergrid <- "3_results/output-data/model_daily/GBM_grid-search"
+out_dirpath_hypergrid <- "3_results/output-data/model_daily/LGB_grid-search"
 if(!dir.exists(out_dirpath_hypergrid)) dir.create(out_dirpath_hypergrid , recursive = TRUE)
 hyper_grid %>%
   write_csv(sprintf("%s/hyper_evaluation.csv" , out_dirpath_hypergrid))
